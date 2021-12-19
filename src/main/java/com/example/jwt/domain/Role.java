e2364e19641932845762dbbfa2754f4d28b0ee00
oussama <abz1999abz@gmail.com>
2021-12-19 12:38:33 +0100

    domain creation

---
+++ src/main/java/com/example/jwt/domain/Role.java
@@ -0,0 +1,18 @@
+package com.example.jwt.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Data
+@AllArgsConstructor
+public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+}
